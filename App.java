65382bc89acdc858b3192000c426299228510325
wendiyu <36240636+wendiyu@users.noreply.github.com>
2019-06-18 18:23:27 -0400

    Update App.java

--- App.java
+++ App.java
@@ -1,17 +1,25 @@
+// using awt to create GUI design
 import java.awt.EventQueue;
 
+// Creat game windows/appliation
 
 public class App {	
 	
 	public static GUI gui;
 	
 	public static void main(String[] args) {
+		
 		EventQueue.invokeLater(new Runnable() { 
 			public void run() {
+				
 				try {
 					gui = new GUI();
+					
 					gui.createGameWindow();
+					
+				// Diagnosing an exceptions
 				} catch (Exception e) {
+					
 					e.printStackTrace();
 				}
 			}
